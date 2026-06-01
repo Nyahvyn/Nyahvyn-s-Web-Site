9bf26a30a71e9b316d175e4b940b3e4a17e80060
Nyahvyn <264426440+Nyahvyn@users.noreply.github.com>
2026-06-01 21:59:34 +0900

    Update and rename main.js to theme.js

---
+++ src/javascrts/theme.js
@@ -0,0 +1,26 @@
+const themebutton = document.querySelector(".theme");
+const html = document.documentElement;
+const icon = document.querySelector(".theme img");
+
+if (!localStorage.getItem("theme")) {
+  localStorage.setItem("theme", "light");
+} else {
+  if (localStorage.getItem("theme") === "dark") {
+    html.classList.replace("light", "dark");
+    icon.src = "../imgs/dark-mode.svg";
+  } 
+}
+
+themebutton.addEventListener("click", () => {
+  if(html.classList.contains("light")) {
+    html.classList.replace("light", "dark");
+    icon.src = "../imgs/dark-mode.svg";
+    localStorage.setItem("theme", "dark");
+    
+  } else {
+    html.classList.replace("dark", "light");
+    icon.src = "../imgs/light-mode.svg"
+    localStorage.setItem("theme", "light");
+    
+  }
+})
