7b9f14fd4bc022c062bd785ac2ee4d1952e2d19f
Nyahvyn <264426440+Nyahvyn@users.noreply.github.com>
2026-06-27 22:55:23 +0900

    Increase scroll width by 20 pixels

--- docs/javascripts/scrollSpeed.js
+++ docs/javascripts/scrollSpeed.js
@@ -1,6 +1,6 @@
 const loop = document.querySelector(".loop");
 const items = document.querySelector(".items");
-const width = items.scrollWidth;
+const width = items.scrollWidth + 20;
 const clone = items.cloneNode(true);
 loop.appendChild(clone);
 
